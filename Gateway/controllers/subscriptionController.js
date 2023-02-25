2361cb0d10e3cd52f94aaced85fb14c031616ea8
anurag1905-hub <anuragharsh626@gmail.com>
2023-02-26 00:07:03 +0530

    update subscription controller

--- Gateway/controllers/subscriptionController.js
+++ Gateway/controllers/subscriptionController.js
@@ -4,8 +4,7 @@ const Buyer = require('../models/Buyer');
 
 module.exports.subscribe = async function(req,res){
     try{
-        console.log(req.body);
-        if(req.body.isSeller){
+        if(JSON.parse(req.body.isSeller)){
             let seller = await Seller.create(req.body);
             return res.status(200).json({
                 data:seller,
@@ -30,14 +29,16 @@ module.exports.subscribe = async function(req,res){
 
 module.exports.unsubscribe = async function(req,res){
     try{
-        let Id = req.params.id;
-        let isSeller = req.params.isSeller;
+        let Id = req.body.Id;
+        let isSeller = JSON.parse(req.body.isSeller);
+
+        //console.log(Id,isSeller);
         if(isSeller){
             let user = await Seller.findById(Id);
             if(user){
                 // Remove the products of the seller
                 await Product.deleteMany({seller:Id});
-                Product.save();
+                
                 // Remove the seller Info
                 user.remove();
 
